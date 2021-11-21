5868ce6f0e58fbd7d594f0ff4cc04deb33a7ccd1
Henrique Cunha <henriquecterrichers@icloud.com>
2021-11-21 18:43:23 -0300

    Atualizando Principal

--- src/main/java/br/com/senac/projetoIntegrador/projectFinal/principal/Principal.java
+++ src/main/java/br/com/senac/projetoIntegrador/projectFinal/principal/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
 	
 	public static void main(String[] args) {
 
+//Iniciando questões na classe principal		
 		Questoes um     = new Questoes();
 		Questoes dois   = new Questoes();
 		Questoes tres   = new Questoes();
@@ -15,24 +16,35 @@ public class Principal {
 		Questoes sete   = new Questoes();
 		Questoes oito   = new Questoes();
 		Questoes nove   = new Questoes();
-		Questoes dez    = new Questoes();		
+		Questoes dez    = new Questoes();
+		
+//Iniciando Scanner para obter respostas do jogador		
+		Scanner input = new Scanner(System.in);
+		
+//Local para armazenar o nome do jogador 
+		String nome;
+
+//Local para armazenar as respostas do jogador
+		String resposta;
+	
+//Iniciando classe menu para execução de métodos		
+		Menu menu = new Menu();
+		
+//Iniciando série de pontuação	
+		int pontuacao = 0;
+
+//na classe player
+		Player jogador = new Player();
 		
 //Inicio Programa
-		
-		Scanner input = new Scanner(System.in);
-		String resposta;
-		int pontuacao = 0;
-		
-		
-		Player jogador = new Player();
-		jogador.nome = "Henrique";
 
+		System.out.println("Por favor, digite seu nome: ");
+		nome = input.nextLine();
+		jogador.nome = nome;
 		System.out.println("Olá " + jogador.nome);
-
-		resposta = input.nextLine();
-		resposta.equalsIgnoreCase("verde");
 		
-		System.out.println("Se quiser jogar, digite verde");
+		resposta = input.nextLine();		
+
 	
 		if(resposta.equals(um.getVerde())) {
 			System.out.println("Simm ");
